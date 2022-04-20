190ec28a2a2d6bd046bc85f4ce48edc74a80f747
Chris Juchem <chrisjuchem15@gmail.com>
2022-04-20 18:43:01 -0400

    text windows library

--- prototype/src/main.cpp
+++ prototype/src/main.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+#include <ncurses.h>
+
+
 int main(int argc, char** argv) {
-    return 33;
+    initscr();         /* Start curses mode */
+    refresh();
+
+    WINDOW* win = newwin(10, 10, 0, 0);
+    WINDOW* inner = newwin(8, 8, 1, 1);
+    box(win, 0, 0);
+    wprintw(inner, "Hello World !!!");
+    wrefresh(win);
+    wrefresh(inner);
+
+    getch();            /* Wait for user input */
+    endwin();           /* Start curses mode */
+
+
+    std::cout << "Lorem Ipsum!" << std::endl; 
+    return 0;
 }
